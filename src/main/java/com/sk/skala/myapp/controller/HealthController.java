2215b034f0e2d2feb423ba851e3e680d4a1e1db4
skala <skala>
2025-04-29 16:27:29 +0900

    Update pom.xml

--- src/main/java/com/sk/skala/myapp/controller/HealthController.java
+++ src/main/java/com/sk/skala/myapp/controller/HealthController.java
@@ -1,3 +1,15 @@
+package com.sk.skala.myapp.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 @RequestMapping("/health")
 public class HealthController {
